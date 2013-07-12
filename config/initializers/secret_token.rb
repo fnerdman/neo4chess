7ed30b3a93b1f7bd6f2b2c0a29259ae9a7233145
lead4good <frieder.paape@rwth-aachen.de>
2013-07-12 13:13:41 +0200

    raw project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neo4chess::Application.config.secret_token = 'c9ef2b019d01f789bbdc55abaa9c69de9622f31d9e053c0c4d5aca72964dbf55958074e693b52040234f90a8faf5d3e489ba9acbadf315de1a47bf9251c8da24'
